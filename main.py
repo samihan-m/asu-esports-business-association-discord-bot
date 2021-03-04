b0dc11cf7c106772553f7f0f00359043aac4f018
PikaGoku <ssmuppirala@gmail.com>
2021-03-04 00:19:29 -0700

    Made Selenium work for Heroku

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ from creds import DISCORD_TOKEN
 
 if __name__ == '__main__':
     
+    #information for making selenium work on heroku
+    import os
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    '''
     #initialize selenium webdriver
     #pointing to chromedriver file
     path_to_webdriver = r'./chromedriver.exe'
@@ -28,6 +39,7 @@ if __name__ == '__main__':
     options.binary_location = "D:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
     #initializing driver
     driver = webdriver.Chrome(executable_path=path_to_webdriver, options=options)
+    '''
     
     def readListings(driver):
         '''
